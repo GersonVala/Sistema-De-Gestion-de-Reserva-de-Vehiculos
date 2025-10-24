0946abae91cc0addfa8cb6fb14f12cb9bc2b563f
GersonVala <gersonvalashek@gmail.com>
2025-10-24 01:16:56 -0300

    Primer commit: Estructura del proyecto

---
+++ src/main/java/com/reservaDeVehiculos/ProyectoReservaDeVehiculos/ProyectoReservaDeVehiculos/ProyectoReservaDeVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.reservaDeVehiculos.ProyectoReservaDeVehiculos.ProyectoReservaDeVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoReservaDeVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoReservaDeVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoReservaDeVehiculos/ProyectoReservaDeVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoReservaDeVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoReservaDeVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
